59f24f12aec02d4d16d6895cc7049852e58c60cc
Gerhard Fabian Hirschfeld <gfhirschfeld@openwt.com>
2018-02-27 19:11:37 +0100

    Add cross origin

--- src/main/java/ch/fhnw/edu/wodss/tippspielapi/controllers/APIController.java
+++ src/main/java/ch/fhnw/edu/wodss/tippspielapi/controllers/APIController.java
@@ -1,6 +1,7 @@
 package ch.fhnw.edu.wodss.tippspielapi.controllers;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,6 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api")
 public class APIController {
 
+    @CrossOrigin
     @GetMapping("")
     public APIInfo getAPIInfo() {
         return new APIInfo("wodss-tippspiel-web","0.0.0");
