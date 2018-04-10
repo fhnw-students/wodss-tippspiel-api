1b6caf323df0f58cc7bf0f84eb6eb896adb0bfec
keniseli <ken.iseli@gmail.com>
2018-04-10 17:53:26 +0000

    Fixed filter to properly return a 401 in case of wrong authentication method for a path

--- src/main/java/ch/fhnw/edu/wodss/tippspielapi/TippspielApi/config/authentication/AuthenticationPathFilter.java
+++ src/main/java/ch/fhnw/edu/wodss/tippspielapi/TippspielApi/config/authentication/AuthenticationPathFilter.java
@@ -8,6 +8,7 @@ import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
 
@@ -16,6 +17,8 @@ public class AuthenticationPathFilter implements Filter {
 
   public static final String BEARER = "Bearer";
   public static final String AUTHORIZATION = "Authorization";
+  public static final String AUTH_LOGIN_PATH = "/auth/login";
+  public static final String BASIC = "Basic";
 
   @Override
   public void init(FilterConfig filterConfig) throws ServletException {
@@ -28,13 +31,13 @@ public class AuthenticationPathFilter implements Filter {
     String authorization = servletRequest.getHeader(AUTHORIZATION);
     String contextPath = servletRequest.getServletPath();
     boolean servletPathIsAuthLogin =
-        contextPath.contains("/auth/login") || contextPath.contains("/auth/login/");
+        contextPath.contains(AUTH_LOGIN_PATH) || contextPath.contains(AUTH_LOGIN_PATH + "/");
     boolean isTokenAuth = authorization.contains(BEARER);
     boolean isAuthLoginPathWithBearerToken = servletPathIsAuthLogin && isTokenAuth;
-    boolean isBasicAuth = authorization.contains("Basic Auth");
+    boolean isBasicAuth = authorization.contains(BASIC);
     boolean isNotAuthLoginPathWithBasicAuth = !servletPathIsAuthLogin && isBasicAuth;
     if (isAuthLoginPathWithBearerToken || isNotAuthLoginPathWithBasicAuth) {
-      SecurityContextHolder.getContext().setAuthentication(null);
+      ((HttpServletResponse) response).sendError(HttpServletResponse.SC_UNAUTHORIZED);
     }
     chain.doFilter(request, response);
   }
